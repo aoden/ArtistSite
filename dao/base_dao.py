1693521d59a32069bbc9c884b8fd2c54f3979035
khoi <le.khoi992@gmail.com>
2016-01-15 14:43:51 +0700

    init

--- dao/base_dao.py
+++ dao/base_dao.py
@@ -1,15 +1,15 @@
-from dao import mysql
+from dao import db_connector
 
 __author__ = 'khoi'
 
 
 class BaseDao:
     def __init__(self):
-        self.connection = mysql.get_connection()
+        self.connection = db_connector.get_connection()
 
     def get_connection(self):
         if not self.connection.open:
-            self.connection = mysql.get_connection()
+            self.connection = db_connector.get_connection()
         return self.connection
 
 
