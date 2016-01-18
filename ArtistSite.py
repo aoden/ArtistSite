3d4f5dafe2eefa877663eefd1b56a18e4b108ce7
khoi <le.khoi992@gmail.com>
2016-01-18 20:13:47 +0700

    init

--- ArtistSite.py
+++ ArtistSite.py
@@ -1,16 +1,21 @@
 from flask import Flask
+from flask.ext.restful.reqparse import RequestParser
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self, dkm):
-        return {dkm: dkm}
+
+class Login(Resource):
+    def post(self):
+        parseer = RequestParser()
+        parseer.add_argument('email', type=str)
+        parseer.add_argument('password', type=str)
+        return parseer.parse_args()
 
 
-api.add_resource(HelloWorld, '/<string:dkm>')
+api.add_resource(Login, '/login')
 
 if __name__ == '__main__':
     app.run(debug=True)
